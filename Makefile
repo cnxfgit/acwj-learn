c5eeac71fb054833368ebdb887360e8b534e0b59
hlx <1198816371@qq.com>
2023-12-12 20:48:25 +0800

    all

--- Makefile
+++ Makefile
@@ -1,28 +1,52 @@
-SRCS= cg.c decl.c expr.c gen.c main.c misc.c scan.c stmt.c sym.c tree.c
-SRCN= cgn.c decl.c expr.c gen.c main.c misc.c scan.c stmt.c sym.c tree.c
+# Define the location of the include directory
+# and the location to install the compiler binary
+INCDIR=/tmp/include
+BINDIR=/tmp
 
-comp1: $(SRCS)
-	cc -o comp1 -g $(SRCS)
+HSRCS= data.h decl.h defs.h incdir.h
+SRCS= cg.c decl.c expr.c gen.c main.c misc.c \
+	opt.c scan.c stmt.c sym.c tree.c types.c
 
-compn: $(SRCN)
-	cc -o compn -g $(SRCN)
+cwj: $(SRCS) $(HSRCS)
+	cc -o cwj -g -Wall $(SRCS)
+
+incdir.h:
+	echo "#define INCDIR \"$(INCDIR)\"" > incdir.h
+
+install: cwj
+	mkdir -p $(INCDIR)
+	rsync -a include/. $(INCDIR)
+	cp cwj $(BINDIR)
+	chmod +x $(BINDIR)/cwj
 
 clean:
-	rm -f comp1 compn *.o *.s out
+	rm -f cwj cwj[0-9] *.o *.s *.q out a.out incdir.h
 
-test: comp1 tests/runtests
+test: install tests/runtests
 	(cd tests; chmod +x runtests; ./runtests)
 
-test6: comp1 tests/input06
-	./comp1 tests/input06
-	cc -o out out.s
-	./out
+# Run the tests, stop on the first failure
+stoptest: install tests/runtests
+	(cd tests; chmod +x runtests; ./runtests stop)
 
-testn: compn tests/runtestsn
-	(cd tests; chmod +x runtestsn; ./runtestsn)
+# Run the tests with the
+# compiler that compiled itself
+test2: install tests/runtests2 cwj2
+	(cd tests; chmod +x runtests2; ./runtests2)
 
-test6n: compn tests/input06
-	./compn tests/input06
-	nasm -f elf64 out.s
-	cc -no-pie -o out out.o
-	./out
+# Try to do the triple test
+triple: cwj3
+	size cwj[23]
+
+# Paranoid: quadruple test
+quad: cwj4
+	size cwj[234]
+
+cwj4: cwj3 $(SRCS) $(HSRCS)
+	./cwj3 -o cwj4 $(SRCS)
+
+cwj3: cwj2 $(SRCS) $(HSRCS)
+	./cwj2 -o cwj3 $(SRCS)
+
+cwj2: install $(SRCS) $(HSRCS)
+	./cwj  -o cwj2 $(SRCS)
--- scan.c
+++ scan.c
@@ -8,15 +8,16 @@
 // Return the position of character c
 // in string s, or -1 if c not found
 static int chrpos(char *s, int c) {
-  char *p;
-
-  p = strchr(s, c);
-  return (p ? p - s : -1);
+  int i;
+  for (i = 0; s[i] != '\0'; i++)
+    if (s[i] == (char) c)
+      return (i);
+  return (-1);
 }
 
 // Get the next character from the input file.
 static int next(void) {
-  int c;
+  int c, l;
 
   if (Putback) {		// Use the character put
     c = Putback;		// back if there is one
@@ -25,8 +26,34 @@ static int next(void) {
   }
 
   c = fgetc(Infile);		// Read from input file
-  if ('\n' == c)
+
+  while (Linestart && c == '#') {	// We've hit a pre-processor statement
+    Linestart = 0;		// No longer at the start of the line
+    scan(&Token);		// Get the line number into l
+    if (Token.token != T_INTLIT)
+      fatals("Expecting pre-processor line number, got:", Text);
+    l = Token.intvalue;
+
+    scan(&Token);		// Get the filename in Text
+    if (Token.token != T_STRLIT)
+      fatals("Expecting pre-processor file name, got:", Text);
+
+    if (Text[0] != '<') {	// If this is a real filename
+      if (strcmp(Text, Infilename))	// and not the one we have now
+	Infilename = strdup(Text);	// save it. Then update the line num
+      Line = l;
+    }
+
+    while ((c = fgetc(Infile)) != '\n');	// Skip to the end of the line
+    c = fgetc(Infile);		// and get the next character
+    Linestart = 1;		// Now back at the start of the line
+  }
+
+  Linestart = 0;		// No longer at the start of the line
+  if ('\n' == c) {
     Line++;			// Increment line count
+    Linestart = 1;		// Now back at the start of the line
+  }
   return (c);
 }
 
@@ -48,21 +75,141 @@ static int skip(void) {
   return (c);
 }
 
-// Scan and return an integer literal
-// value from the input file. Store
-// the value as a string in Text.
-static int scanint(int c) {
-  int k, val = 0;
+// Read in a hexadecimal constant from the input
+static int hexchar(void) {
+  int c, h, n = 0, f = 0;
 
+  // Loop getting characters
+  while (isxdigit(c = next())) {
+    // Convert from char to int value
+    h = chrpos("0123456789abcdef", tolower(c));
+
+    // Add to running hex value
+    n = n * 16 + h;
+    f = 1;
+  }
+
+  // We hit a non-hex character, put it back
+  putback(c);
+
+  // Flag tells us we never saw any hex characters
+  if (!f)
+    fatal("missing digits after '\\x'");
+  if (n > 255)
+    fatal("value out of range after '\\x'");
+
+  return (n);
+}
+
+// Return the next character from a character
+// or string literal
+static int scanch(void) {
+  int i, c, c2;
+
+  // Get the next input character and interpret
+  // metacharacters that start with a backslash
+  c = next();
+  if (c == '\\') {
+    switch (c = next()) {
+      case 'a':
+	return ('\a');
+      case 'b':
+	return ('\b');
+      case 'f':
+	return ('\f');
+      case 'n':
+	return ('\n');
+      case 'r':
+	return ('\r');
+      case 't':
+	return ('\t');
+      case 'v':
+	return ('\v');
+      case '\\':
+	return ('\\');
+      case '"':
+	return ('"');
+      case '\'':
+	return ('\'');
+
+	// Deal with octal constants by reading in
+	// characters until we hit a non-octal digit.
+	// Build up the octal value in c2 and count
+	// # digits in i. Permit only 3 octal digits.
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+	for (i = c2 = 0; isdigit(c) && c < '8'; c = next()) {
+	  if (++i > 3)
+	    break;
+	  c2 = c2 * 8 + (c - '0');
+	}
+
+	putback(c);		// Put back the first non-octal char
+	return (c2);
+      case 'x':
+	return (hexchar());
+      default:
+	fatalc("unknown escape sequence", c);
+    }
+  }
+  return (c);			// Just an ordinary old character!
+}
+
+// Scan and return an integer literal
+// value from the input file.
+static int scanint(int c) {
+  int k, val = 0, radix = 10;
+
+  // Assume the radix is 10, but if it starts with 0
+  if (c == '0') {
+    // and the next character is 'x', it's radix 16
+    if ((c = next()) == 'x') {
+      radix = 16;
+      c = next();
+    } else
+      // Otherwise, it's radix 8
+      radix = 8;
+
+  }
   // Convert each character into an int value
-  while ((k = chrpos("0123456789", c)) >= 0) {
-    val = val * 10 + k;
+  while ((k = chrpos("0123456789abcdef", tolower(c))) >= 0) {
+    if (k >= radix)
+      fatalc("invalid digit in integer literal", c);
+    val = val * radix + k;
     c = next();
   }
 
   // We hit a non-integer character, put it back.
   putback(c);
   return (val);
+}
+
+// Scan in a string literal from the input file,
+// and store it in buf[]. Return the length of
+// the string. 
+static int scanstr(char *buf) {
+  int i, c;
+
+  // Loop while we have enough buffer space
+  for (i = 0; i < TEXTLEN - 1; i++) {
+    // Get the next char and append to buf
+    // Return when we hit the ending double quote
+    if ((c = scanch()) == '"') {
+      buf[i] = 0;
+      return (i);
+    }
+    buf[i] = (char) c;
+  }
+
+  // Ran out of buf[] space
+  fatal("String literal too long");
+  return (0);
 }
 
 // Scan an identifier from the input file and
@@ -77,10 +224,11 @@ static int scanident(int c, char *buf, int lim) {
     if (lim - 1 == i) {
       fatal("Identifier too long");
     } else if (i < lim - 1) {
-      buf[i++] = c;
+      buf[i++] = (char) c;
     }
     c = next();
   }
+
   // We hit a non-valid character, put it back.
   // NUL-terminate the buf[] and return the length
   putback(c);
@@ -94,9 +242,33 @@ static int scanident(int c, char *buf, int lim) {
 // to waste time strcmp()ing against all the keywords.
 static int keyword(char *s) {
   switch (*s) {
+    case 'b':
+      if (!strcmp(s, "break"))
+	return (T_BREAK);
+      break;
+    case 'c':
+      if (!strcmp(s, "case"))
+	return (T_CASE);
+      if (!strcmp(s, "char"))
+	return (T_CHAR);
+      if (!strcmp(s, "continue"))
+	return (T_CONTINUE);
+      break;
+    case 'd':
+      if (!strcmp(s, "default"))
+	return (T_DEFAULT);
+      break;
     case 'e':
       if (!strcmp(s, "else"))
 	return (T_ELSE);
+      if (!strcmp(s, "enum"))
+	return (T_ENUM);
+      if (!strcmp(s, "extern"))
+	return (T_EXTERN);
+      break;
+    case 'f':
+      if (!strcmp(s, "for"))
+	return (T_FOR);
       break;
     case 'i':
       if (!strcmp(s, "if"))
@@ -104,9 +276,35 @@ static int keyword(char *s) {
       if (!strcmp(s, "int"))
 	return (T_INT);
       break;
-    case 'p':
-      if (!strcmp(s, "print"))
-	return (T_PRINT);
+    case 'l':
+      if (!strcmp(s, "long"))
+	return (T_LONG);
+      break;
+    case 'r':
+      if (!strcmp(s, "return"))
+	return (T_RETURN);
+      break;
+    case 's':
+      if (!strcmp(s, "sizeof"))
+	return (T_SIZEOF);
+      if (!strcmp(s, "static"))
+	return (T_STATIC);
+      if (!strcmp(s, "struct"))
+	return (T_STRUCT);
+      if (!strcmp(s, "switch"))
+	return (T_SWITCH);
+      break;
+    case 't':
+      if (!strcmp(s, "typedef"))
+	return (T_TYPEDEF);
+      break;
+    case 'u':
+      if (!strcmp(s, "union"))
+	return (T_UNION);
+      break;
+    case 'v':
+      if (!strcmp(s, "void"))
+	return (T_VOID);
       break;
     case 'w':
       if (!strcmp(s, "while"))
@@ -116,11 +314,35 @@ static int keyword(char *s) {
   return (0);
 }
 
+// List of token strings, for debugging purposes
+char *Tstring[] = {
+  "EOF", "=", "+=", "-=", "*=", "/=", "%=",
+  "?", "||", "&&", "|", "^", "&",
+  "==", "!=", "<", ">", "<=", ">=", "<<", ">>",
+  "+", "-", "*", "/", "%", "++", "--", "~", "!",
+  "void", "char", "int", "long",
+  "if", "else", "while", "for", "return",
+  "struct", "union", "enum", "typedef",
+  "extern", "break", "continue", "switch",
+  "case", "default", "sizeof", "static",
+  "intlit", "strlit", ";", "identifier",
+  "{", "}", "(", ")", "[", "]", ",", ".",
+  "->", ":"
+};
+
 // Scan and return the next token found in the input.
 // Return 1 if token valid, 0 if no tokens left.
 int scan(struct token *t) {
   int c, tokentype;
 
+  // If we have a lookahead token, return this token
+  if (Peektoken.token != 0) {
+    t->token = Peektoken.token;
+    t->tokstr = Peektoken.tokstr;
+    t->intvalue = Peektoken.intvalue;
+    Peektoken.token = 0;
+    return (1);
+  }
   // Skip whitespace
   c = skip();
 
@@ -131,16 +353,53 @@ int scan(struct token *t) {
       t->token = T_EOF;
       return (0);
     case '+':
-      t->token = T_PLUS;
+      if ((c = next()) == '+') {
+	t->token = T_INC;
+      } else if (c == '=') {
+	t->token = T_ASPLUS;
+      } else {
+	putback(c);
+	t->token = T_PLUS;
+      }
       break;
     case '-':
-      t->token = T_MINUS;
+      if ((c = next()) == '-') {
+	t->token = T_DEC;
+      } else if (c == '>') {
+	t->token = T_ARROW;
+      } else if (c == '=') {
+	t->token = T_ASMINUS;
+      } else if (isdigit(c)) {	// Negative int literal
+	t->intvalue = -scanint(c);
+	t->token = T_INTLIT;
+      } else {
+	putback(c);
+	t->token = T_MINUS;
+      }
       break;
     case '*':
-      t->token = T_STAR;
+      if ((c = next()) == '=') {
+	t->token = T_ASSTAR;
+      } else {
+	putback(c);
+	t->token = T_STAR;
+      }
       break;
     case '/':
-      t->token = T_SLASH;
+      if ((c = next()) == '=') {
+	t->token = T_ASSLASH;
+      } else {
+	putback(c);
+	t->token = T_SLASH;
+      }
+      break;
+    case '%':
+      if ((c = next()) == '=') {
+	t->token = T_ASMOD;
+      } else {
+	putback(c);
+	t->token = T_MOD;
+      }
       break;
     case ';':
       t->token = T_SEMI;
@@ -157,6 +416,30 @@ int scan(struct token *t) {
     case ')':
       t->token = T_RPAREN;
       break;
+    case '[':
+      t->token = T_LBRACKET;
+      break;
+    case ']':
+      t->token = T_RBRACKET;
+      break;
+    case '~':
+      t->token = T_INVERT;
+      break;
+    case '^':
+      t->token = T_XOR;
+      break;
+    case ',':
+      t->token = T_COMMA;
+      break;
+    case '.':
+      t->token = T_DOT;
+      break;
+    case ':':
+      t->token = T_COLON;
+      break;
+    case '?':
+      t->token = T_QUESTION;
+      break;
     case '=':
       if ((c = next()) == '=') {
 	t->token = T_EQ;
@@ -169,12 +452,15 @@ int scan(struct token *t) {
       if ((c = next()) == '=') {
 	t->token = T_NE;
       } else {
-	fatalc("Unrecognised character", c);
+	putback(c);
+	t->token = T_LOGNOT;
       }
       break;
     case '<':
       if ((c = next()) == '=') {
 	t->token = T_LE;
+      } else if (c == '<') {
+	t->token = T_LSHIFT;
       } else {
 	putback(c);
 	t->token = T_LT;
@@ -183,13 +469,44 @@ int scan(struct token *t) {
     case '>':
       if ((c = next()) == '=') {
 	t->token = T_GE;
+      } else if (c == '>') {
+	t->token = T_RSHIFT;
       } else {
 	putback(c);
 	t->token = T_GT;
       }
       break;
+    case '&':
+      if ((c = next()) == '&') {
+	t->token = T_LOGAND;
+      } else {
+	putback(c);
+	t->token = T_AMPER;
+      }
+      break;
+    case '|':
+      if ((c = next()) == '|') {
+	t->token = T_LOGOR;
+      } else {
+	putback(c);
+	t->token = T_OR;
+      }
+      break;
+    case '\'':
+      // If it's a quote, scan in the
+      // literal character value and
+      // the trailing quote
+      t->intvalue = scanch();
+      t->token = T_INTLIT;
+      if (next() != '\'')
+	fatal("Expected '\\'' at end of char literal");
+      break;
+    case '"':
+      // Scan in a literal string
+      scanstr(Text);
+      t->token = T_STRLIT;
+      break;
     default:
-
       // If it's a digit, scan the
       // literal integer value in
       if (isdigit(c)) {
@@ -201,7 +518,7 @@ int scan(struct token *t) {
 	scanident(c, Text, TEXTLEN);
 
 	// If it's a recognised keyword, return that token
-	if (tokentype = keyword(Text)) {
+	if ((tokentype = keyword(Text)) != 0) {
 	  t->token = tokentype;
 	  break;
 	}
@@ -214,5 +531,6 @@ int scan(struct token *t) {
   }
 
   // We found a token
+  t->tokstr = Tstring[t->token];
   return (1);
 }
